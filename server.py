4ca2211991ad0d4ac7c0c28c74d606c0fadf21d5
Marise Hayashi <vyatheslav2707@mail.ru>
2017-11-20 21:25:06 +0300

    New cards

--- server.py
+++ server.py
@@ -26,7 +26,20 @@ def index():
         'sony': "data:image/png;base64," + b64encode(open(f'./static/images/codes/sony/{choice(listdir(abspath("./static/images/codes/sony/")))}', 'rb').read()).decode("utf-8").strip("b"),
         'samsung': "data:image/png;base64," + b64encode(open(f'./static/images/codes/samsung/{choice(listdir(abspath("./static/images/codes/samsung/")))}', 'rb').read()).decode("utf-8").strip("b"),
         'funday': "data:image/png;base64," + b64encode(open(f'./static/images/codes/funday/{choice(listdir(abspath("./static/images/codes/funday/")))}', 'rb').read()).decode("utf-8").strip("b"),
-        'nike': "data:image/png;base64," + b64encode(open(f'./static/images/codes/nike/{choice(listdir(abspath("./static/images/codes/nike/")))}', 'rb').read()).decode("utf-8").strip("b")
+        'nike': "data:image/png;base64," + b64encode(open(f'./static/images/codes/nike/{choice(listdir(abspath("./static/images/codes/nike/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'evrasia': "data:image/png;base64," + b64encode(open(f'./static/images/codes/evrasia/{choice(listdir(abspath("./static/images/codes/evrasia/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'eldorado': "data:image/png;base64," + b64encode(open(f'./static/images/codes/eldorado/{choice(listdir(abspath("./static/images/codes/eldorado/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        '4itay_gorod': "data:image/png;base64," + b64encode(open(f'./static/images/codes/4itay_gorod/{choice(listdir(abspath("./static/images/codes/4itay_gorod/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        '5tero4ka': "data:image/png;base64," + b64encode(open(f'./static/images/codes/5tero4ka/{choice(listdir(abspath("./static/images/codes/5tero4ka/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'adidas': "data:image/png;base64," + b64encode(open(f'./static/images/codes/adidas/{choice(listdir(abspath("./static/images/codes/adidas/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'burger_king': "data:image/png;base64," + b64encode(open(f'./static/images/codes/burger_king/{choice(listdir(abspath("./static/images/codes/burger_king/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'carusel': "data:image/png;base64," + b64encode(open(f'./static/images/codes/carusel/{choice(listdir(abspath("./static/images/codes/carusel/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'cinema_park': "data:image/png;base64," + b64encode(open(f'./static/images/codes/cinema_park/{choice(listdir(abspath("./static/images/codes/cinema_park/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'fixprice': "data:image/png;base64," + b64encode(open(f'./static/images/codes/fixprice/{choice(listdir(abspath("./static/images/codes/fixprice/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'maxidom': "data:image/png;base64," + b64encode(open(f'./static/images/codes/maxidom/{choice(listdir(abspath("./static/images/codes/maxidom/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'mediamarkt': "data:image/png;base64," + b64encode(open(f'./static/images/codes/mediamarkt/{choice(listdir(abspath("./static/images/codes/mediamarkt/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'mvideo': "data:image/png;base64," + b64encode(open(f'./static/images/codes/mvideo/{choice(listdir(abspath("./static/images/codes/mvideo/")))}', 'rb').read()).decode("utf-8").strip("b"),
+        'okey': "data:image/png;base64," + b64encode(open(f'./static/images/codes/okey/{choice(listdir(abspath("./static/images/codes/okey/")))}', 'rb').read()).decode("utf-8").strip("b")
     }
 
     response = make_response(render_template("index.html", barcodes=barcodes_dict))
